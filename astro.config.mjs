3e2069e036ecf3d6a69c92add3e2eaabdcc52226
AniSanProductions <alejandrosandana22@gmail.com>
2026-08-19 21:36:27 -0400

    Add Astro GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://anisanproductions.com',
   integrations: [react()]
 });
